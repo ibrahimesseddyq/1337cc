a04490f6d1c7cd146035bcf713bfd0aad9c9d979
Ibra <ibrahim.essedyq@gmail.com>
2025-08-08 14:45:49 +0100

    preparing the lexer

--- compiler.h
+++ compiler.h
@@ -2,11 +2,49 @@
  #define COMPILER_H 
 
 #include <stdio.h>
+#include <stdbool.h>
 
+struct pos
+{
+    int line;
+    int col;
+    const char *filename;
+};
+
+enum 
+{
+    TOKEN_TYPE_IDENTIFIER,
+    TOKEN_TYPE_KEYWORD,
+    TOKEN_TYPE_OPERATOR,
+    TOKEN_TYPE_SYMBOL,
+    TOKEN_TYPE_NUMBER,
+    TOKEN_TYPE_STRING,
+    TOKEN_TYPE_COMMENT,
+    TOKEN_TYPE_NEWLINE
+     
+};
+struct lex_process;
+typedef char (*LEX_PROCESS_NEXT_CHAR)(struct lex_process* process);
+struct lex_process_function
+{
+
+};
+struct lex_process
+{
+    struct pos pos;
+    struct vector* token_vec;
+    struct compile_process* compiler;
+
+    int current_expression_count;// how many bracket 
+    struct buffer* parentheses_buffer;
+    struct lex_process_function* function;
+
+    void* private;
+};
 struct token 
 {
     int type;
-    int flags;
+    int flags; 
 
     union 
     {
@@ -19,6 +57,9 @@ struct token
     };
     // True if their whitespace is between the token and the next one
     bool whitespace;
+
+    // for debugging issue
+    const char *between_brackets;
 };
 
 
